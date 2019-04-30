0ee6767d856009bcfaacd6378c3fce50c61ea2a2
Nathan Pierce <nathan.pierce@block.one>
2019-04-30 14:01:27 -0400

    fixes

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -4,10 +4,12 @@ set -eo pipefail
 PATH=$PATH:~/opt/mongodb/bin
 echo "[Extracting build directory]"
 [[ -z "${1}" ]] && tar -zxf build.tar.gz || tar -xzf $1
+echo "[Killing old MongoDB]"
+$(pgrep mongod | xargs kill -9) || true
 echo "[Starting MongoDB]"
-mongod --fork --dbpath ~/data/mongodb -f ~/etc/mongod.conf --logpath "$(pwd)"/mongod.log
+mongod --fork --dbpath ~/data/mongodb -f ~/etc/mongod.conf --logpath ~/var/log/mongodb/mongod.log
 cd ./build
 # run tests
 echo "[Running tests]"
-ctest -L nonparallelizable_tests --output-on-failure -T Test
+ctest -R nodeos_run_test --output-on-failure -T Test
 mv $(pwd)/Testing/$(ls $(pwd)/Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
