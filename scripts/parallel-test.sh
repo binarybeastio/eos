6f8e951c81b273d0cf48babfe311e574292771e6
Nathan Pierce <nathan.pierce@block.one>
2019-04-30 21:54:30 -0400

    fixes

--- scripts/parallel-test.sh
+++ scripts/parallel-test.sh
@@ -9,6 +9,7 @@ ctest -j $(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test
 EXIT_STATUS=$?
 [[ "$EXIT_STATUS" == 0 ]] && set -e
 # Prepare tests for artifact upload
-mv $(pwd)/Testing/$(ls $(pwd)/Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+echo $?
 # ctest error handling
 [[ "$EXIT_STATUS" != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS" && exit $EXIT_STATUS
