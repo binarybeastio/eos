07f2f4ccba324f27e6f01230da1174d37eb03d2f
Nathan Pierce <nathan.pierce@block.one>
2019-05-01 15:24:41 -0400

    fixes

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -12,7 +12,9 @@ echo "[Running tests]"
 set +e # defer ctest error handling to end
 ctest -L nonparallelizable_tests --output-on-failure -T Test
 EXIT_STATUS=$?
-[[ "$EXIT_STATUS" == 0 ]] && set -e
+[[ $EXIT_STATUS == 0 ]] && set -e
 mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+echo $?
+echo $EXIT_STATUS
 # ctest error handling
-[[ "$EXIT_STATUS" != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS" && exit $EXIT_STATUS
+[[ $EXIT_STATUS != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS" && exit $EXIT_STATUS
