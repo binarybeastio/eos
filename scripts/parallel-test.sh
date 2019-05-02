ae48cc778fd04c5d927a0fd21ea4bfa27d38c044
Nathan Pierce <nathan.pierce@block.one>
2019-05-01 23:24:19 -0400

    fixes

--- scripts/parallel-test.sh
+++ scripts/parallel-test.sh
@@ -8,7 +8,5 @@ EXIT_STATUS=$?
 [[ "$EXIT_STATUS" == 0 ]] && set -e
 # Prepare tests for artifact upload
 mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
-echo $?
-echo $EXIT_STATUS
 # ctest error handling
-[[ "$EXIT_STATUS" != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS" && exit $EXIT_STATUS
+[[ $EXIT_STATUS > 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"; exit $EXIT_STATUS
