68b47509643ec1d421713c33f5b424360300b8a5
Nathan Pierce <nathan.pierce@block.one>
2019-05-02 14:20:36 -0400

    fixes

--- scripts/parallel-test.sh
+++ scripts/parallel-test.sh
@@ -8,5 +8,8 @@ EXIT_STATUS=$?
 [[ "$EXIT_STATUS" == 0 ]] && set -e
 # Prepare tests for artifact upload
 mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+buildkite-agent artifact upload test-results.xml
+buildkite-agent artifact upload config.ini
+buildkite-agent artifact upload genesis.json
 # ctest error handling
 [[ $EXIT_STATUS != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"; exit $EXIT_STATUS
