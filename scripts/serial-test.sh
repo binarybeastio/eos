3d509702c496269114dad80fdca73c9c82351eab
Nathan Pierce <nathan.pierce@block.one>
2019-05-02 10:10:41 -0400

    fixes

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -14,5 +14,6 @@ ctest -L nonparallelizable_tests --output-on-failure -T Test
 EXIT_STATUS=$?
 [[ $EXIT_STATUS == 0 ]] && set -e
 mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+mv ~/var/log/mongodb/mongod.log ./
 # ctest error handling
 [[ $EXIT_STATUS > 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"; exit $EXIT_STATUS
